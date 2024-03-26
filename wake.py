f564438092a98c6b4ff3f197fe7b78224ec7f23a
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-03-26 14:39:44 +0300

    Update wake.py

--- wake.py
+++ wake.py
@@ -10,7 +10,7 @@ def dencrypt(cipherText):
     # XOR with
     "sdfor XORed with a ciphertext stream to produce plaintext. And it’s fast. WAKE works in CFB; "
     return plainText     
-def GenerateKey(text):         
+def GenerateKey(text):   
     # ai+1 = M(ai ,di)  
     # bi+1 = M(bi ,ai+1)
     # ci+1 = M(ci,bi+1) 
