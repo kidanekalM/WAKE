01f9a20ebc75c8a54f0860be2c2eeab4fb5e319f
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-03-26 10:24:07 +0300

    update wake

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from encrypt import wake
+# from wake import encrypt
     
-plainText = input("Enter the text")
-cipherText = encrypt(plainText)
-print("The cipher text is :",cipherText)
+# plainText = input("Enter the text: ")
+# cipherText = encrypt(plainText)
+# print("The cipher text is :",cipherText)
+
+n=1
+while True:
+    print(n)
+    if((((13*n))%60)==1):
+        print(n)
+        break
+    else:
+        n=n+1
--- wake.py
+++ wake.py
@@ -1,21 +1,34 @@
-def encrypt(plainText):  
+def encrypt(plainText): 
+    # Produce S - box
+    # Generate KeyStream 1 use CFB
+    # XOR with plaintext 
     for i in range(0,10):
         print ()
     return plainText
 def dencrypt(cipherText):
+    # Generate KeyStram 1 use CFB 
+    # XOR with 
+    "sdfor XORed with a ciphertext stream to produce plaintext. And it’s fast. WAKE works in CFB; "
     return plainText     
-
+def XORi(text):         
+    # ai+1 = M(ai ,di)  
+    # bi+1 = M(bi ,ai+1)
+    # ci+1 = M(ci,bi+1) 
+    # di+1 = M(di ,ci+1)
+    # Function M is     
+    # M(x,y) = (x + y) >> 8 • S(x+y) ^ 255
+    return
 def M(x,y):
     return 
 """
 17.3 WAKE
-WAKE is the Word Auto Key Encryption algorithm, invented by David        
-Wheeler [1589]. 
+WAKE is the Word Auto Key Encryption algorithm, invented by David
+Wheeler [1589].                                                  
 It produces a stream of 32-bit words which can be XORed  
 with a plaintext stream to produce ciphertext,
-
-or XORed with a ciphertext stream to produce plaintext. And it’s fast.                              
-WAKE works in CFB; 
+                                              
+or XORed with a ciphertext stream to produce plaintext. And it’s fast.
+WAKE works in CFB;                                                    
 the previous ciphertext word is used to generate the next key word.
  It also uses an S-box of 256 32-bit values. This S-box has a     
 special property: The high-order byte of all the entries is a permutation of all
@@ -23,7 +36,7 @@ possible bytes, and the low-order 3 bytes are random.
 First, generate the S-box entries, Si, from the key. Then initialize four registers
 with the key (or with another key): a0, b0 , c0, and d0. To generate a 32-bit      
 keystream word, Ki:
-Ki = di
+Ki = di            
 The ciphertext word Ci, is the plaintext word, Pi XORed with Ki.
 Then, update the four registers:
 ai+1 = M(ai ,di)  
