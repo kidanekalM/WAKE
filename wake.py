c9a52a13d4db8ec37dfc26fae44b52b1b8f344ee
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-13 11:05:16 +0300

    Fixed bugs related to decrypt

--- wake.py
+++ wake.py
@@ -1,3 +1,15 @@
+from sbox import sBlock 
+def calculate_next_value(R, prevR):
+    R_decimal = int(R, 16)
+    prevR_decimal = int(prevR, 16)
+
+    sum_val = R_decimal + prevR_decimal
+    S_index = sum_val & 255
+    S_value = sBlock[S_index]  # Assuming get_S_value is defined elsewhere
+    result = (sum_val >> 8) ^ S_value
+
+    return '{:08X}'.format(result)
+
 def get_hex_numeric_sequence(input_string):
     numeric_sequence = []
     for c in input_string:
@@ -5,11 +17,12 @@ def get_hex_numeric_sequence(input_string):
         numeric_sequence.append(char_value)
     return numeric_sequence
 
-def get_next_key(plain_key):
+def get_auto_key(plain_key):
     # Get hex key
     hexadecimal_result = []
     for c in plain_key:
-        hex_value = format(ord(c), 'X2')
+        #hex_value = format(ord(c), 'X2')
+        hex_value ='{:02X}'.format(ord(c))
         hexadecimal_result.append(hex_value)
     hex_key = ''.join(hexadecimal_result)
     if len(hex_key) < 16:
@@ -20,7 +33,6 @@ def get_next_key(plain_key):
     R4 = hex_key[8:16]
     R5 = hex_key[16:24]
     R6 = hex_key[24:32] 
-
     R3_next = calculate_next_value(R3, R6)
     R4_next = calculate_next_value(R4, R3_next)
     R5_next = calculate_next_value(R5, R4_next)
@@ -31,10 +43,11 @@ def get_next_key(plain_key):
 
 def decrypt(cipher_text,key):
     encrypted_numeric = get_hex_numeric_sequence(cipher_text)
-    key_numeric = get_next_key(key)
+    key_numeric = get_hex_numeric_sequence(get_auto_key(key))
 
     decrypted_numeric = []
     for i in range(len(encrypted_numeric)):
+        #print("aaa",encrypted_numeric[i],type( key_numeric[i % len(key_numeric)]))
         decrypted_char = encrypted_numeric[i] ^ key_numeric[i % len(key_numeric)]
         decrypted_numeric.append(decrypted_char)
 
@@ -43,7 +56,7 @@ def decrypt(cipher_text,key):
     return decrypted_text
 
 def encrypt(plaintext,input_key):
-    key = get_next_key(input_key)
+    key = get_auto_key(input_key)
 
     plaintext_numeric = get_hex_numeric_sequence(plaintext)
     key_numeric = get_hex_numeric_sequence(key)
@@ -67,3 +80,4 @@ def encrypt(plaintext,input_key):
     return encrypted_text
 
 encrypt("aaaaaaaaaaaaaa","aaaaaaaaaaaaaaaa")
+decrypt('QQWP"R RQQWP"R',"aaaaaaaaaaaaaaaa")
