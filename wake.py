231f660956ec104faa775ea5200d7f8d24416a1e
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-13 09:01:59 +0300

    added decrypt

--- wake.py
+++ wake.py
@@ -4,3 +4,18 @@ def get_hex_numeric_sequence(input_string):
         char_value = ord(c)
         numeric_sequence.append(char_value)
     return numeric_sequence
+
+def decrypt(cipher_text,key):
+    encrypted_text = cipher_text
+
+    encrypted_numeric = get_hex_numeric_sequence(encrypted_text)
+    key_numeric = get_hex_numeric_sequence(key)
+
+    decrypted_numeric = []
+    for i in range(len(encrypted_numeric)):
+        decrypted_char = encrypted_numeric[i] ^ key_numeric[i % len(key_numeric)]
+        decrypted_numeric.append(decrypted_char)
+
+    decrypted_text = ''.join(chr(num) for num in decrypted_numeric)
+
+    return decrypted_text
